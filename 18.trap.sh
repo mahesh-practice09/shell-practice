6c9fb03ecffdc2ae9a772b7f9f2372bb80feef28
mahesh <mahesh050917@gmail.com>
2026-02-15 14:46:35 +0530

    shell scripting

--- 18.trap.sh
+++ 18.trap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -o pipefail
-#set -e
+set -e
 trap 'echo "Script have been Interrupted by pressing CTRL+C"; exit 1' SIGINT
 trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
