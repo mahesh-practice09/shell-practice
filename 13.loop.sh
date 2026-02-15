e9a59fc639050fe48ed06401fed85f54cf9dc502
mahesh <mahesh050917@gmail.com>
2026-02-15 12:25:14 +0530

    shell scripting

---
+++ 13.loop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o pipefail
+
+LOGS_FOLDER=/var/log/Shell-Script
+LOGS_File=/var/log/Shell-Script/$0.log
+
+Userid=$(id -u)
+
+if [ $Userid != 0 ]; then
+   echo "please run the script with sudo permissions" 
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALDIATE() {
+    if [ $1 -eq 0 ]; then
+    echo "$2...Success" 
+    else
+    echo "$2...Failure" 
+    fi
+}
+
+for package in $@
+   do
+     dnf install $package -y &>> $LOGS_File
+     VALDIATE $? "Installing $package"
+   done
