e3a3339c287e0b21f6dad03d74783f41fed57391
mahesh <mahesh050917@gmail.com>
2026-02-15 12:38:52 +0530

    shell scripting

---
+++ 15.colours.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+echo -e "$R Hello World $N"
+echo -e "$B This is Mahesh $N"
+echo -e "$Y This is Keeru $N"
+echo -e "$G This is Parnika $N"
+echo -e "$G This is jeevika $N"
