bdf00799c873fc86a7ee2d5f46e4c3dbcf8764e2
mahesh <mahesh050917@gmail.com>
2026-02-15 11:55:11 +0530

    shell scripting

---
+++ 10.Functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+if [ $Userid != 0 ]; then
+   echo "please run the script with sudo permissions"
+   exit 1
+fi
+
+VALDIATE() {
+    if [$1 -eq 0 ]; then
+    echo "$2...Success"
+    else
+    echo "$2...Failure"
+    fi
+}
+
+
+dnf install nginx -y
+VALDIATE $? , Install nginx
+
+
