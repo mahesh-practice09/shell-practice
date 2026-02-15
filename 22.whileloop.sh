12aba6fda080c0285af8a31819df1a1587e4cf86
mahesh <mahesh050917@gmail.com>
2026-02-15 18:56:28 +0530

    shell scripting

--- 22.whileloop.sh
+++ 22.whileloop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-count=1
+# count=1
 
-while [ $count -le 5 ]
-do
-echo "count is $count"
-sleep 1
-((count++))
-done
+# while [ $count -le 5 ]
+# do
+# echo "count is $count"
+# sleep 1
+# ((count++))
+# done
+
+while IFS=read -r line; do
+echo "Line: $line"
+done < 20.script.sh
